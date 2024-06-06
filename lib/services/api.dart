3440d9b103f65010825469e3bf75be9e03f36288
Artiyan Prasetya <artiyan.prasetya@munalively.com>
2024-06-06 20:37:02 +0700

    add api services

---
+++ lib/services/api.dart
@@ -0,0 +1,35 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:pokemon_app/model/pokemon.dart';
+
+class PokemonAPI {
+  Future<List<Pokemon>?> getPokemonList() async {
+    List<Pokemon> pokemonList = [];
+
+    final response =
+        await http.get(Uri.parse("https://pokeapi.co/api/v2/pokemon"));
+
+    if (response.statusCode == 200) {
+      Map<String, dynamic> jsonResponse = jsonDecode(response.body);
+
+      for (var pokemon in jsonResponse['results']) {
+        pokemonList.add(Pokemon.fromJson(pokemon));
+      }
+
+      return pokemonList;
+    } else {
+      throw Exception('Failed to load pokemon list');
+    }
+  }
+
+  Future<Pokemon?> getDetailPokemon(String url) async {
+    final response = await http.get(Uri.parse(url));
+
+    if (response.statusCode == 200) {
+      Map<String, dynamic> jsonResponse = jsonDecode(response.body);
+      return Pokemon.fromJson(jsonResponse);
+    } else {
+      throw Exception('Failed to load pokemon');
+    }
+  }
+}
